d93547dac47cb0eb75f4b0d7e80021b20d970c5e
Heyveryouaire <mathieu.pavy.29@gmail.com>
2022-12-19 12:01:05 +0100

    Optimisation: -36 bits

--- xprs.js
+++ xprs.js
@@ -1,6 +1,6 @@
-const Joi = require ('joi');
-const express = require('express');
-const app = express();
+const Joi = require ('joi')
+const express = require('express')
+const app = express()
 
 app.use(express.json()); // adding a middleware
 
@@ -14,12 +14,12 @@ const courses = [
 
 
 app.get('/', (req, res)=> {
-    res.send('Hello World!');
+    res.send('Hello World!')
 });
 
 
 app.get('/api/courses', (req, res)=> {
-    res.send(courses);
+    res.send(courses)
 });
 
 app.post('/api/courses', (req, res) => {
@@ -38,16 +38,16 @@ app.post('/api/courses', (req, res) => {
     // we don not nedd what was written befor anymore
     // we replace it with the validateCourse() function=======
    
-    const { error } = validateCourse(req.body); // { error } = result.error
+    const { error } = validateCourse(req.body) // { error } = result.error
     //400 Bad request
-    if (error) return res.status(400).send(error);
+    if (error) return res.status(400).send(error)
 
     const course = {
         id: courses.length + 1,
         name: req.body.name
-    };
+    }
     courses.push(course);
-    res.send(course);
+    res.send(course)
 });
 
 
@@ -57,30 +57,30 @@ app.put('/api/courses/:id', (req, res) => {
     //look up the course
     // return 404 if not exist
 
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The id was not found');
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('The id was not found')
 
 
     //vvalidate
     //if invalid , return 400 = bad request
    
     // const result = validateCourse(req.body); // to make cleaner = obj destructuring
-    const { error } = validateCourse(req.body); // { error } = result.error
+    const { error } = validateCourse(req.body) // { error } = result.error
     //400 Bad request
-    if (error) return res.status(400).send(error);
+    if (error) return res.status(400).send(error)
 
     // update course
-    course.name = req.body.name;
+    course.name = req.body.name
 
     // return the updated course
-    res.send(course);
+    res.send(course)
 });
 
 function validateCourse(course){
     const schema = {
         name: Joi.string().min(2).required()
-    };
-    return Joi.validate(course, schema);
+    }
+    return Joi.validate(course, schema)
 }
 
 // app.get('/api/courses/:id', (req, res)=> {
@@ -98,27 +98,27 @@ function validateCourse(course){
 
 app.delete('/api/courses/:id', (req, res) => {
     // look for the course
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The id was not found');
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('The id was not found')
 
     // delete
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
+    const index = courses.indexOf(course)
+    courses.splice(index, 1)
 
     // return the same course
-    res.send(course);
+    res.send(course)
 });
 
 app.get('/api/courses/:id', (req, res)=> {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The id was not found');
-    res.send(course);
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) return res.status(404).send('The id was not found')
+    res.send(course)
 });
 
 
-const port = process.env.PORT || 3000; // to set dynamic port
+const port = process.env.PORT || 3000 // to set dynamic port
 
-app.listen(port, ()=> console.log(`listening on ${port}`));
+app.listen(port, ()=> console.log(`listening on ${port}`))
 
 // to change server port from 3000 to another port
 // in the terminal we can use : export PORT=5000 or any other arbitrary number
